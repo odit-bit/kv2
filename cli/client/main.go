02eb23f2050349feb8b94c453afd12f5ca156eec
odit-bit <geadwi@gmail.com>
2024-06-13 15:08:41 +0700

    modified ./cli/client for health command

--- cli/client/main.go
+++ cli/client/main.go
@@ -6,7 +6,16 @@ import (
 )
 
 func main() {
+	var (
+		port string
+		host string
+	)
+
 	app := cobra.Command{}
+
+	app.PersistentFlags().StringVarP(&port, "port", "p", "6969", "port bind")
+	app.PersistentFlags().StringVar(&host, "host", "", "host name")
+
 	app.AddCommand(&cmd.SetCMD, &cmd.GetCMD, &cmd.Health)
 	app.Execute()
 }
